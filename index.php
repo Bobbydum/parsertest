909a2c114cb97aa55abb168ff7b7b3d5fe48e6bc
andrey <kotulski@gmail.com>
2016-03-25 14:00:51 +0200

    php7 sugar

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require('vendor/autoload.php');
 
-//use App\Classes\Security\Provider\{
-//    ApiKeyUserServiceProvider,
-//    ApiKeyServiceProvider
-//};
+use App\Classes\Security\Provider\{
+    ApiKeyUserServiceProvider,
+    ApiKeyServiceProvider
+};
 use Silex\Application;
 use Silex\Provider\{
     SerializerServiceProvider,
@@ -21,20 +21,20 @@ $config = include_once 'config/config.php';
 $app['users'] = $config['users'];
 
 $app->register(new SerializerServiceProvider());
-//$app->register(new ApiKeyUserServiceProvider());
-//$app->register(new ApiKeyServiceProvider());
-//$app->register(new SecurityServiceProvider(), array(
-//    'security.firewalls' => array(
-//        'api' => array(
-//            'apikey' => true,
-//            'pattern' => '^/vf/ru/rest',
-//            'stateless' => false
-//        )
-//    ),
-//    'security.access_rules' => array(
-//        array(new RequestMatcher('^/vf/ru/rest'), array('ROLE_USER'))
-//    )
-//));
+$app->register(new ApiKeyUserServiceProvider());
+$app->register(new ApiKeyServiceProvider());
+$app->register(new SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'api' => array(
+            'apikey' => true,
+            'pattern' => '^/vf/ru/rest',
+            'stateless' => false
+        )
+    ),
+    'security.access_rules' => array(
+        array(new RequestMatcher('^/vf/ru/rest'), array('ROLE_USER'))
+    )
+));
 
 
 $app->get('/', 'App\Classes\First::index');
