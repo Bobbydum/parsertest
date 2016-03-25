46dfd447c837a57e71a7acb423e9258da4ca1fd0
andrey <kotulski@gmail.com>
2016-03-25 09:33:02 +0200

    IDE to ignore

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'users' => array(
+        'samsung' => 'LMj4D8p1ADO8ggDzRWenykZZamBxjyE1'
+    )
+);
--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
 require('vendor/autoload.php');
 
+//use App\Classes\Security\Provider\{
+//    ApiKeyUserServiceProvider,
+//    ApiKeyServiceProvider
+//};
 use Silex\Application;
-use Silex\Provider\SerializerServiceProvider;
-use App\Classes\Security\Provider\ApiKeyUserServiceProvider;
-use App\Classes\Security\Provider\ApiKeyServiceProvider;
-use Symfony\Component\HttpFoundation\RequestMatcher;
-use Silex\Provider\SecurityServiceProvider;
+use Silex\Provider\{
+    SerializerServiceProvider,
+    SecurityServiceProvider
+};
+
+
 
 $app = new Application();
 $app['debug'] = true;
@@ -15,26 +20,24 @@ $config = include_once 'config/config.php';
 
 $app['users'] = $config['users'];
 
-$app->register(new ApiKeyUserServiceProvider());
-$app->register(new ApiKeyServiceProvider());
-$app->register(new SecurityServiceProvider(), array(
-    'security.firewalls' => array(
-        'api' => array(
-            'apikey' => true,
-            'pattern' => '^/vf/ru/rest',
-            'stateless' => false
-        )
-    ),
-    'security.access_rules' => array(
-        array(new RequestMatcher('^/vf/ru/rest'), array('ROLE_USER'))
-    )
-));
-
 $app->register(new SerializerServiceProvider());
+//$app->register(new ApiKeyUserServiceProvider());
+//$app->register(new ApiKeyServiceProvider());
+//$app->register(new SecurityServiceProvider(), array(
+//    'security.firewalls' => array(
+//        'api' => array(
+//            'apikey' => true,
+//            'pattern' => '^/vf/ru/rest',
+//            'stateless' => false
+//        )
+//    ),
+//    'security.access_rules' => array(
+//        array(new RequestMatcher('^/vf/ru/rest'), array('ROLE_USER'))
+//    )
+//));
+
 
 $app->get('/', 'App\Classes\First::index');
 $app->get('/first', 'App\Classes\First::index');
-
-
 
 $app->run(); // Запуск приложения
